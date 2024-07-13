9df5f3fe3d61695fac5d5265201769d39cc6b07c
protofu <sungjae0512@gmail.com>
2024-07-13 16:26:57 +0900

    algo : study

---
+++ kukdas_algo/src/com/algo/Boj2566.java
@@ -0,0 +1,25 @@
+package com.algo;
+
+import java.util.Scanner;
+
+public class Boj2566 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int maxNum = 0, x = 1, y = 1;
+	
+		for (int i=0;i<9;i++) {
+			for (int j=0;j<9;j++) {
+				int n = sc.nextInt();
+				if (maxNum < n) {
+					maxNum = n;
+					x = i+1;
+					y = j+1;
+				}
+			}
+		}
+		System.out.println(maxNum);
+		System.out.println(x+" "+y);
+
+		sc.close();
+	}
+}
